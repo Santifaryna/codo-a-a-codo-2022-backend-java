361b3ec8c009a6b2a3399435a0c79915e4a85428
santifaryna <santifaryna@gmail.com>
2022-07-04 15:45:07 -0300

    Clase 27

---
+++ src/main/java/codo/a/codo/Variables.java
@@ -0,0 +1,22 @@
+package codo.a.codo;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		char letra = 'A';
+		String palabra = "hola soy carlos"
+				
+		byte b = 100;
+		short s = 10000;
+		int i = 1000000000;
+		long l = 1000000000;
+		
+		//numéricos con decimales
+        float f = 1.0f; 
+        double d = 1.0;
+        
+        boolean flag = false;
+        
+	}
+
+}
