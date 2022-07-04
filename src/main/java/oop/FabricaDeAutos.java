75af8733f35f66d2b0251fd337e31390f9c65649
santifaryna <santifaryna@gmail.com>
2022-07-04 15:52:21 -0300

    Clase 27

---
+++ src/main/java/oop/FabricaDeAutos.java
@@ -0,0 +1,7 @@
+package oop;
+
+public class FabricaDeAutos {
+    public static void main(String[] args) {
+    	Auto fiatUno
+    }
+}
---
+++ src/main/java/oop/auto.java
@@ -0,0 +1,29 @@
+package oop;
+
+public class auto {
+	string color;
+	int anio;
+	long serie;
+	string marca;
+	string modelo;
+	float velocidad;
+	float velocidadMaxima;
+	boolean encendido;
+
+	// metodos
+	void encender() {
+
+	}
+
+	void apagar() {
+
+	}
+
+	void acelerar() {
+
+	}
+
+	void frenar() {
+
+	}
+}
